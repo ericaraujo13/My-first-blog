686ac3abaa5efaf191f4821583f12d8d13fca9d5
Eric Araujo <ericploni@gmail.com>
2022-06-15 10:40:06 -0300

    add fix config i18-n

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.i18n.default_locale = 'pt_BR'
+    config.i18n.default_locale = 'pt-BR'
     config.time_zone = 'Brasilia'
     # Configuration for the application, engines, and railties goes here.
     #
